b38df64d63d045ed594e0ee79f3fd4f7bb8ec92e
MUGABE Robertson <139157186+mugabe-rob@users.noreply.github.com>
2024-06-15 22:30:15 +0200

    A project that defines the structure of online commerce

---
+++ src/assets/assets.js
@@ -0,0 +1,25 @@
+import menu_1 from './1.jpg'
+import menu_2 from './2.jpg'
+import menu_3 from './3.jpg'
+import menu_4 from './4.jpg'
+import menu_5 from './beef.jpg'
+import menu_6 from './beer.jpg'
+import menu_7 from './chicken.jpg'
+import menu_8 from './wine.jpg'
+import logo from './logo.svg'
+import search from './search-icon.png'
+import basket_icon from './cart.png'
+
+export const assets = {
+    menu_1,
+    menu_2,
+    menu_3, 
+    menu_4,
+    menu_5,
+    menu_6,
+    menu_7,
+    menu_8,
+    logo,
+    search,
+    basket_icon
+}
