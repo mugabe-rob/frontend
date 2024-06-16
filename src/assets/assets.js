b1f5ab7aa9339b09d70165d574213e39322bf4f2
MUGABE Robertson <139157186+mugabe-rob@users.noreply.github.com>
2024-06-16 21:49:21 +0200

    Modifying the front-end section

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,7 +1,7 @@
 import menu_1 from './1.jpg'
 import menu_2 from './2.jpg'
 import menu_3 from './3.jpg'
-import menu_4 from './4.jpg'
+
 import menu_5 from './beef.jpg'
 import menu_6 from './beer.jpg'
 import menu_7 from './chicken.jpg'
@@ -15,7 +15,6 @@ export const assets = {
     menu_1,
     menu_2,
     menu_3, 
-    menu_4,
     menu_5,
     menu_6,
     menu_7,
@@ -25,3 +24,33 @@ export const assets = {
     basket_icon,
     bag
 }
+export const menu_list = [
+    {
+        menu_name: "Italian Dish",
+        menu_image: menu_1,
+    },
+    {
+        menu_name: "American Salad",
+        menu_image: menu_2,
+    },
+    {
+        menu_name: "Turkish Kebab",
+        menu_image: menu_3,
+    },
+    {
+        menu_name: "Chinese Beef",
+        menu_image: menu_5,
+    },
+    {
+        menu_name: "Beer",
+        menu_image: menu_6,
+    },
+    {
+        menu_name: "Chicken",
+        menu_image: menu_7,
+    },
+    {
+        menu_name: "British Cocktail",
+        menu_image: menu_8,
+    }
+]
